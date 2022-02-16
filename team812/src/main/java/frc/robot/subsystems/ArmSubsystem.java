d184b10226acaba586361c29eccd8f483ba7047d
Team 812! <preuss@team812.com>
2022-02-15 17:39:43 -0800

    Removed unused import

--- team812/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ team812/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,7 +5,6 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.Constants.CANConstants;
